191cbbcfda4ab3c6c58e9894d68a9ac2618af188
manishcodes-dot <manishrana98449@gmail.com>
2026-02-07 16:27:46 +0545

    first code

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('hello world');
+
+let name = 'mosh';
+console.log(name);
+
+
+let FirstName = 'rana', firstName = 'mansih';
+const jack = 'miss';
+let number = 18;
+let iam = undefined;
+let selected = null;
+
+console.log(firstName, FirstName, jack, number, iam, selected);
+
