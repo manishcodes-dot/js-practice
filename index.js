579893e14a960c21c619b422410c2015c337490a
manishcodes-dot <manishrana98449@gmail.com>
2026-02-23 22:58:58 +0545

    updated code

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-console.log('hello world');
+// console.log('hello world');
 
-let name = 'mosh';
-console.log(name);
+// let name = 'mosh';
+// console.log(name);
 
 
-let FirstName = 'rana', firstName = 'mansih';
-const jack = 'miss';
-let number = 18;
-let iam = undefined;
-let selected = null;
+// let FirstName = 'rana', firstName = 'mansih';
+// const jack = 'miss';
+// let number = 18;
+// let iam = undefined;
+// let selected = null;
 
-console.log(firstName, FirstName, jack, number, iam, selected);
+// console.log(firstName, FirstName, jack, number, iam, selected);
+
+
+// // This is used to find the last letter of the first name.
+// // var firtName = 'rana';  
+// // var lasletteroffirstName = firtName[firstName.length - 1];
+
+
+// //arrays
+
 
